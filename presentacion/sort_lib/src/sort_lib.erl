99c6f9dd8c921c93ddd664316ef89f010f14af2e
Miguel E. Ruiz <meruiz77@gmail.com>
2021-12-08 11:54:24 +0100

    Biblioteca usada en la presentacion de ACFI

--- presentacion/sort_lib/src/sort_lib.erl
+++ presentacion/sort_lib/src/sort_lib.erl
@@ -1,7 +1,7 @@
 -module(sort_lib).
 
 %% API exports
--export([sort/1, ordered/1]).
+-export([sort/1, delete/2, ordered/1]).
 
 %%====================================================================
 %% API functions
@@ -10,7 +10,18 @@
 -spec sort(list(integer())) -> list(integer()).
 sort([]) -> [];
 sort([P|Xs]) ->
-  sort([X || X <- Xs, X < P]) ++ [P] ++ sort([X || X <- Xs, P =< X]).
+  sort([X || X <- Xs, X < P]) ++ [P] ++ sort([X || X <- Xs, P < X]).
+
+% A lists delete implementation
+-spec delete(integer(), list(integer())) -> list(integer()).
+delete(X, L) ->
+  delete(X, L, []).
+delete(_, [], Acc) ->
+  lists:reverse(Acc);
+delete(X, [X|Rest], Acc) ->
+  lists:reverse(Acc) ++ Rest;
+delete(X, [Y|Rest], Acc) ->
+  delete(X, Rest, [Y|Acc]).
 
 -spec ordered(list(integer())) -> boolean().
 ordered([]) -> true;
